9ec48d13e322348c88ffb5c83f30bc8862d24457
oparah nkume nwachukwu <103194129+nwachukwuoparah@users.noreply.github.com>
2024-03-25 17:39:01 +0100

    update on butto

--- src/components/delete/index.tsx
+++ src/components/delete/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "@/styles/delete.module.css";
-import Button from "../button";
+import Button from "@/components/button";
 import {
 	QueryFilters,
 	useMutation,
